56308ba7489221d09c15924b99de221d49946546
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-01-30 20:33:26 -0500

    finish my commit

--- TP1/Exercise3.py
+++ TP1/Exercise3.py
@@ -1,4 +1,4 @@
-
+import math
 def  exercise3():
     n = -1 
 
@@ -56,4 +56,53 @@ def exercise4():
        
 
 
-exercise4()
+def mod_vect(x,y,z):
+    Distance = math.sqrt(x*x + y*y + z*z)
+    print ("La distance de ce point à l'origine est:")
+    print (Distance)
+    return Distance
+
+def exo5():
+    print ("Veuillez entrer un point en x :")
+    x = int(input())
+    print ("Veuillez entrer un point en y :")
+    y = int(input()) 
+    print ("Veuillez entrer un point en z :")
+    z = int(input())
+    plus_proche_de_lorigine = mod_vect(x,y,z)
+    while plus_proche_de_lorigine != 0:
+        print ("Veuillez entrer un point en x :")
+        x = int(input())
+        print ("Veuillez entrer un point en y :")
+        y = int(input()) 
+        print ("Veuillez entrer un point en z :")
+        z = int(input())
+        dist_nouveau_point = mod_vect(x,y,z)
+        if  dist_nouveau_point < plus_proche_de_lorigine:
+            print("Ce point est plus proche de l'rogine que les point precedent")
+            plus_proche_de_lorigine=dist_nouveau_point
+
+    print("vous avez trouvez le point le plus proche de l'origine")
+
+
+def f(y,x):
+    return (y+x/y)/2 
+
+def exo6():
+    print("Entrez un réel positif x")
+    x = int(input()) 
+    y0=x
+    i=1
+
+    e=1
+    while  e > 0.01:
+        racine=f(i,x)
+        if (racine>i):
+            e=racine-i
+        else:
+            e=i-racine
+        i=racine
+    print("La racine carré de x est: ")
+    print(i)  
+
+exo6()
