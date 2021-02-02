f4246b381117b68d8701292ed4a8cea98e9e8000
jamesley joseph <jamesleyjoseph@jamesleys-Mac-mini.local>
2021-02-01 22:24:53 -0500

    finish my commit

--- TP2/include/Declaration.h
+++ TP2/include/Declaration.h
@@ -4,3 +4,4 @@ int Exo2();
 int Exo3();
 int Exo4();
 int Exo5();
+float mod_vect(int x,int y, int z);
--- TP2/src/Exo4.cpp
+++ TP2/src/Exo4.cpp
@@ -1,40 +1,39 @@
 #include <iostream>
 #include <string>
 
+float mod_vect(int x, int y, int z)
+{
+    int Distance = sqrt(x * x + y * y + z * z);
+    std::cout << "La distance de ce point à l'origine est:" << std::endl
+              << Distance << std::endl;
+    return Distance;
+}
+
 int Exo4()
 {
-    std::cout << "veillier entrer un phrase" << std::endl;
-    std::string phrase;
-    std::cin >> phrase;
-
-    std::string Inf = "INF";
-
-    int i = 0;
-    int correctPosition = 0;
-    while (i < phrase.length())
+    int x, y, z;
+    std::cout << "Veuillez entrer un point en x :" << std::endl;
+    std::cin >> x;
+    std::cout << "Veuillez entrer un point en y :" << std::endl;
+    std::cin >> y;
+    std::cout << "Veuillez entrer un point en z :" << std::endl;
+    std::cin >> z;
+    float plus_proche_de_lorigine = mod_vect(x, y, z);
+    while (plus_proche_de_lorigine != 0)
     {
-
-        int temp = i;
-        int j = 0;
-        while (j < Inf.length())
+        std::cout << "Veuillez entrer les coordonnées d'un autre point" << std::endl;
+        std::cout << "Veuillez entrer un point en x :" << std::endl;
+        std::cin >> x;
+        std::cout << "Veuillez entrer un point en y :" << std::endl;
+        std::cin >> y;
+        std::cout << "Veuillez entrer un point en z :" << std::endl;
+        std::cin >> z;
+        float dist_nouveau_point = mod_vect(x, y, z);
+        if (dist_nouveau_point < plus_proche_de_lorigine)
         {
-            if (Inf[j] == phrase[temp])
-            {
-                correctPosition++;
-                j++;
-                temp++;
-            }
-        }
-
-        if (correctPosition == Inf.length())
-        {
-            std::cout << i << std::endl;
-            return 0;
-        }
-        else
-        {
-            correctPosition = 0;
-            i++;
+            std::cout << "Ce point est plus proche de l'rogine que les point precedent" << std::endl;
+            plus_proche_de_lorigine = dist_nouveau_point;
         }
     }
+    std::cout << "vous avez trouvez le point le plus proche de l'origine" << std::endl;
 }
